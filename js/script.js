34073157b7f7971ee7fb1d666c0b4cd7cf8292f1
Luka Tsinaridze <lukatsinaridze@gmail.com>
2022-12-18 19:49:19 +0400

    tsc init

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+"use strict";
 const bmiResult = document.querySelector(`.result`);
 const weight = document.querySelector(`.weight-field`);
 const height = document.querySelector(`.height-field`);
@@ -10,28 +11,30 @@ const bmiScale = document.querySelector(`.scale`);
 const arrowIcon = document.querySelector(`.arrow-icon`);
 const rangeInfo = document.querySelector(`.range-info`);
 (() => {
-  bmiScale.classList.add(`hidden`);
+    bmiScale.classList.add(`hidden`);
 })();
 const caclBMI = function (weight, height) {
-  return weight / (height / 100) ** 2;
+    return weight / (height / 100) ** 2;
 };
 const removeRange = function () {
-  rangeInfo.style.display = `none`;
-  bmiMain.classList.remove(`filter`);
+    rangeInfo.style.display = `none`;
+    bmiMain.classList.remove(`filter`);
 };
 const calcFunction = function () {
-  if (!weight.value || !height.value) return;
-  const BMI = Math.round(caclBMI(+weight.value, +height.value));
-  bmiResult.textContent = BMI.toString();
-  bmiScale.classList.remove(`hidden`);
-  BMI <= 100 ? (bmiLevel.style.left = `${BMI}%`) : (bmiLevel.style.left = `100%`);
+    if (!weight.value || !height.value)
+        return;
+    const BMI = Math.round(caclBMI(+weight.value, +height.value));
+    bmiResult.textContent = BMI.toString();
+    bmiScale.classList.remove(`hidden`);
+    BMI <= 100 ? (bmiLevel.style.left = `${BMI}%`) : (bmiLevel.style.left = `100%`);
 };
 calcBtn.addEventListener(`click`, calcFunction);
 rangeBtn.addEventListener(`click`, function () {
-  rangeInfo.style.display = `flex`;
-  bmiMain.classList.add(`filter`);
+    rangeInfo.style.display = `flex`;
+    bmiMain.classList.add(`filter`);
 });
 closeBtn.addEventListener(`click`, removeRange);
 document.addEventListener(`keydown`, function (e) {
-  if (e.key === `Escape`) removeRange();
+    if (e.key === `Escape`)
+        removeRange();
 });
