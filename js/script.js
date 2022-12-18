8b0ae833e3b9008cc743adf465204b95aa546894
Luka Tsinaridze <lukatsinaridze@gmail.com>
2022-12-18 18:07:33 +0400

    ts

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-`use strict`;
-// consts
 const bmiResult = document.querySelector(`.result`);
 const weight = document.querySelector(`.weight-field`);
 const height = document.querySelector(`.height-field`);
@@ -11,50 +9,29 @@ const bmiMain = document.querySelector(`.bmi-main`);
 const bmiScale = document.querySelector(`.scale`);
 const arrowIcon = document.querySelector(`.arrow-icon`);
 const rangeInfo = document.querySelector(`.range-info`);
-// ######################################################
-// functions
-const calcBMI = function (height, weight) {
+(() => {
+  bmiScale.classList.add(`hidden`);
+})();
+const caclBMI = function (weight, height) {
   return weight / (height / 100) ** 2;
 };
 const removeRange = function () {
   rangeInfo.style.display = `none`;
   bmiMain.classList.remove(`filter`);
 };
-// class manipulation
-bmiScale.classList.add(`hidden`);
-// ######################################################
-// event listener(calculate bmi)
-calcBtn.addEventListener(`click`, function () {
-  if (weight.value && height.value) {
-    const BMI = calcBMI(height.value, weight.value);
-    const bmiRound = Math.round(10 * BMI) / 10;
-    bmiResult.textContent = bmiRound;
-    bmiScale.classList.remove(`hidden`);
-    if (bmiRound <= 96.5) {
-      bmiLevel.style.left = `${bmiRound}%`;
-    } else {
-      bmiLevel.style.left = `96.5%`;
-    }
-  } else {
-    bmiResult.textContent = `Empty`;
-  }
-});
-// ######################################################
-// event listener(open range window)
+const calcFunction = function () {
+  if (!weight.value || !height.value) return;
+  const BMI = Math.round(caclBMI(+weight.value, +height.value));
+  bmiResult.textContent = BMI.toString();
+  bmiScale.classList.remove(`hidden`);
+  BMI <= 100 ? (bmiLevel.style.left = `${BMI}%`) : (bmiLevel.style.left = `100%`);
+};
+calcBtn.addEventListener(`click`, calcFunction);
 rangeBtn.addEventListener(`click`, function () {
   rangeInfo.style.display = `flex`;
   bmiMain.classList.add(`filter`);
 });
-// ######################################################
-// event listener(close range window)
-closeBtn.addEventListener(`click`, function () {
-  removeRange();
-});
-// ######################################################
-// event listener(close range window on keypress(esc))
+closeBtn.addEventListener(`click`, removeRange);
 document.addEventListener(`keydown`, function (e) {
-  if (e.key === `Escape`) {
-    removeRange();
-  }
+  if (e.key === `Escape`) removeRange();
 });
-// ######################################################
---
+++ js/script.ts
@@ -0,0 +1,41 @@
+const bmiResult = document.querySelector(`.result`) as HTMLTextAreaElement;
+const weight = document.querySelector(`.weight-field`) as HTMLInputElement;
+const height = document.querySelector(`.height-field`) as HTMLInputElement;
+const calcBtn = document.querySelector(`.calculate-btn`) as HTMLButtonElement;
+const rangeBtn = document.querySelector(`.range-btn`) as HTMLButtonElement;
+const closeBtn = document.querySelector(`.close`) as HTMLButtonElement;
+const bmiLevel = document.querySelector(`.bmi-level`) as HTMLDivElement;
+const bmiMain = document.querySelector(`.bmi-main`) as HTMLDivElement;
+const bmiScale = document.querySelector(`.scale`) as HTMLDivElement;
+const arrowIcon = document.querySelector(`.arrow-icon`) as HTMLPictureElement;
+const rangeInfo = document.querySelector(`.range-info`) as HTMLDivElement;
+
+(() => {
+  bmiScale.classList.add(`hidden`);
+})();
+const caclBMI = function (weight: number, height: number) {
+  return weight / (height / 100) ** 2;
+};
+const removeRange = function () {
+  rangeInfo.style.display = `none`;
+  bmiMain.classList.remove(`filter`);
+};
+const calcFunction = function (): void {
+  if (!weight.value || !height.value) return;
+  const BMI = Math.round(caclBMI(+weight.value, +height.value));
+  bmiResult.textContent = BMI.toString();
+  bmiScale.classList.remove(`hidden`);
+  BMI <= 100 ? (bmiLevel.style.left = `${BMI}%`) : (bmiLevel.style.left = `100%`);
+};
+calcBtn.addEventListener(`click`, calcFunction);
+
+rangeBtn.addEventListener(`click`, function () {
+  rangeInfo.style.display = `flex`;
+  bmiMain.classList.add(`filter`);
+});
+
+closeBtn.addEventListener(`click`, removeRange);
+
+document.addEventListener(`keydown`, function (e) {
+  if (e.key === `Escape`) removeRange();
+});
